9f6d6dc88a9b67f7bd6e4b258ff618d7f1acc712
dohk <dohk325@gmail.com>
2024-01-04 21:23:46 +0900

    정수의 길이

---
+++ 006-정수의길이/006.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+N = input()
+
+print(len(N))
---
+++ 006-정수의길이/README.md
@@ -0,0 +1,1 @@
+https://level.goorm.io/exam/173089/%EC%A0%95%EC%88%98%EC%9D%98-%EA%B8%B8%EC%9D%B4/quiz/1
