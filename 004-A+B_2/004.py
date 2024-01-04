245c48390b7fe051e7a50fb50fa77c9c2573147d
dohk <dohk325@gmail.com>
2024-01-04 21:16:30 +0900

    A+B(2)

---
+++ 004-A+B_2/004.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+user_input = input()
+
+A, B = user_input.split(' ')
+print("{:.6f}".format(float(A)+float(B)))
---
+++ 004-A+B_2/README.md
@@ -0,0 +1,1 @@
+https://level.goorm.io/exam/174704/a-b-2/quiz/1
