90703c895f251b3b0ba9045e4b3cc444b2a1fcdf
dohk <dohk325@gmail.com>
2024-01-04 21:09:31 +0900

    큰 팩토리얼

---
+++ 003-A+B/003.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+user_input = input()
+
+A, B = user_input.split(' ')
+print(int(A)+int(B))
---
+++ 003-A+B/README.md
@@ -0,0 +1,1 @@
+https://level.goorm.io/exam/174702/a-b/quiz/1
